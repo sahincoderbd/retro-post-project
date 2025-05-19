4fa1f8816949269da810557843baacbd939506d2
sahincoder <sahincoderbd@gmail.com>
2025-05-19 21:43:42 +0600

    post added

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // adjust according to your project structure
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#5201FF',
+        secondary: '#797DFC',
+      },
+    },
+  },
+  plugins: [],
+};

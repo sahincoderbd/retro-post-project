c6dca84aaf8c0d9b09d77bd8704fb0c0a32fad55
sahincoder <sahincoderbd@gmail.com>
2025-05-22 13:09:20 +0600

    latest post displayed

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-console.log('JS file loading');
 
-const loadPostData=async()=>{
+const loadAllPostData=async()=>{
 const res=await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
 const data=await res.json();
 console.log(data.message);
@@ -18,7 +17,7 @@ const displayPosts=(data)=>{
     // console.log(typeof(posts));
   posts.forEach(post => {
     
-   console.log(post);
+  //  console.log(post);
 
       // create post container
 
@@ -136,12 +135,12 @@ const displayPosts=(data)=>{
 
 }
 
-loadPostData();
+loadAllPostData();
 
 //  user active status update
 const userActiveStatusUpdate=()=>{
 const postDiv=document.querySelectorAll('.post-container');
-console.log(postDiv.length);
+// console.log(postDiv.length);
 postDiv.forEach((element)=>{
   
   const isActive=element.getAttribute('isactive')==='true';
@@ -215,4 +214,75 @@ else{
 }
 
 
+/**
+ * Load Latest Post data
+ * 
+ */
 
+const loadLatestPostData=async()=>{
+const response=await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts');
+const data=await response.json();
+displayLatestPosts(data);
+console.log('Latest Post data loaded successfully');
+}
+
+const displayLatestPosts=(latestPosts)=>{
+  const latestPostsContainer=document.getElementById('latestPostsContainer');
+
+  latestPosts.forEach((latestPost)=>{
+ 
+    console.log(latestPost);
+
+    const latestPostChildContainer=document.createElement('div');
+    latestPostChildContainer.innerHTML=`
+    <!-- post -->
+            <div class="flex flex-col gap-3 text-left justify-between items-">
+           
+             <img class="rounded-2xl" src="${latestPost.cover_image}" alt="">
+              <!-- published date -->
+              <div class="flex gap-2 items-center">
+                
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <g clip-path="url(#clip0_29_1881)">
+                  <path d="M4 7C4 6.46957 4.21071 5.96086 4.58579 5.58579C4.96086 5.21071 5.46957 5 6 5H18C18.5304 5 19.0391 5.21071 19.4142 5.58579C19.7893 5.96086 20 6.46957 20 7V19C20 19.5304 19.7893 20.0391 19.4142 20.4142C19.0391 20.7893 18.5304 21 18 21H6C5.46957 21 4.96086 20.7893 4.58579 20.4142C4.21071 20.0391 4 19.5304 4 19V7Z" stroke="#12132D" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                  <path d="M16 3V7" stroke="#12132D" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                  <path d="M8 3V7" stroke="#12132D" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                  <path d="M4 11H20" stroke="#12132D" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                  <path d="M11 16C11 16.2652 11.1054 16.5196 11.2929 16.7071C11.4804 16.8946 11.7348 17 12 17C12.2652 17 12.5196 16.8946 12.7071 16.7071C12.8946 16.5196 13 16.2652 13 16C13 15.7348 12.8946 15.4804 12.7071 15.2929C12.5196 15.1054 12.2652 15 12 15C11.7348 15 11.4804 15.1054 11.2929 15.2929C11.1054 15.4804 11 15.7348 11 16Z" stroke="#12132D" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                  </g>
+                  <defs>
+                  <clipPath id="clip0_29_1881">
+                  <rect width="24" height="24" fill="white"/>
+                  </clipPath>
+                  </defs>
+                  </svg>
+                <span class="text-gray-700 text-lg">${latestPost.author?.posted_date||'Date not Found'}</span>
+
+               
+             </div>
+            <h3 class="text-xl lg:text-2xl text-black font-semibold">${latestPost.title}</h3>
+           <p class="text-lg text-gray-600">${latestPost.description}</p>
+            
+   
+           <!-- post author -->
+           <div class="flex justify-start mt-3 gap-3">
+            <img class="w-[50px] h-[50px] rounded-full" src="${latestPost.profile_image}" alt="autor">
+            <div>
+              <h6 class="text-black text-xl font-medium">${latestPost.author?.name||'Unknown'}</h6>
+              <p class="text-base text-gray-700 ">${latestPost.author?.designation||'Unknown'}</p>
+            </div>
+   
+           </div>
+           </div>
+         
+    `;
+    latestPostChildContainer.setAttribute('class','flex flex-col gap-4 bg-[#797DFC1A] rounded-2xl border-2 border-[#797DFC] p-3 lg:p-4')
+latestPostsContainer.appendChild(latestPostChildContainer);
+
+  });
+//  console.log(latestPosts);
+
+}
+
+
+loadLatestPostData();

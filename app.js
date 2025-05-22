e10f35f7ff10ea97376ef5ce8ea42abe75508ca1
sahincoder <sahincoderbd@gmail.com>
2025-05-22 09:55:13 +0600

    Post and Side bar Post Mark As Read Functionality Added

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const displayPosts=(data)=>{
     // console.log(typeof(posts));
   posts.forEach(post => {
     
-//    console.log(post);
+   console.log(post);
 
       // create post container
 
@@ -41,7 +41,7 @@ const displayPosts=(data)=>{
           <p># <span>${post.category}</span> </p>
           <p>Author: <span>${post.author.name}</span> </p>
           </div>
-         <h3 class="text-xl lg:text-2xl text-black font-semibold">${post.title}</h3>
+         <h3 class="text-xl lg:text-2xl text-black font-semibold " id="post-title-${post.id}">${post.title}</h3>
         <p class="text-lg text-gray-600 border-b-[#12132D40] pb-5 border-b border-dashed">${post.description}</p>
          
 
@@ -61,7 +61,7 @@ const displayPosts=(data)=>{
               <path d="M9.66667 8C9.66667 8.61884 9.9125 9.21233 10.3501 9.64992C10.7877 10.0875 11.3812 10.3333 12 10.3333C12.6188 10.3333 13.2123 10.0875 13.6499 9.64992C14.0875 9.21233 14.3333 8.61884 14.3333 8C14.3333 7.38116 14.0875 6.78767 13.6499 6.35008C13.2123 5.9125 12.6188 5.66667 12 5.66667C11.3812 5.66667 10.7877 5.9125 10.3501 6.35008C9.9125 6.78767 9.66667 7.38116 9.66667 8Z" stroke="#12132D" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
               <path d="M22.5 8C19.7 12.6667 16.2 15 12 15C7.8 15 4.3 12.6667 1.5 8C4.3 3.33333 7.8 1 12 1C16.2 1 19.7 3.33333 22.5 8Z" stroke="#12132D" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
               </svg>
-            <span class="text-gray-700 text-lg">${post.view_count}</span>
+            <span class="text-gray-700 text-lg " id="post-views-count-id-${post.id}">${post.view_count}</span>
             </div>
             <!-- published time -->
             <div class="flex gap-2 items-center">
@@ -75,7 +75,7 @@ const displayPosts=(data)=>{
 
           <!-- mark as read -->
           <div class="flex gap-2 items-center self-end cursor-pointer">
-            <button onClick = "sideBarPostHandler(${post.id})" class="flex gap-2 items-center cursor-pointer markAsRead">
+            <button onClick = "sideBarPostHandler(${post.id})" class="flex gap-2 items-center cursor-pointer">
             <img src="images/email.png">
             </button>
           </div>
@@ -108,47 +108,112 @@ const displayPosts=(data)=>{
        
       `;
 
-      postContainer.setAttribute('class','post-container flex flex-col gap-5  hover:bg-[#797DFC1A] rounded-2xl border-2 border-[#797DFC] p-4');
+      postContainer.setAttribute('class','post-container flex flex-col gap-5 hover:bg-[#797DFC1A] rounded-2xl border-2 border-[#797DFC] p-4');
+      postContainer.setAttribute('id',`${post.id}`);
+      postContainer.setAttribute('isactive',`${post.isActive}`);
       postsParentContainer.appendChild(postContainer);
    
-
-
-  
-  
   // Apply the function for each element
   const postContents=document.querySelectorAll('.post-container');
   postContents.forEach((post)=>{
-     
+    //  Hide the skeleton place holder
     setTimeout(()=>{
           const postSkeletons=post.querySelectorAll('.post-skeleton');
           const postSingles=post.querySelectorAll('.post-single');
          
           postSkeletons.forEach((skeleton)=>skeleton.classList.add('hidden'));
           postSingles.forEach((postSingle)=>postSingle.classList.remove('hidden'));
+
           },2000);
+
+          
    });
   
    
   });
- 
 
+  userActiveStatusUpdate();
 
-}
-
-
-const sideBarPostHandler=(postId)=>{
-
-    console.log(postId);
-   // when user click the envelope icon 
-   const markAsReadAreas=postContainer.getElementsByClassName('markAsRead');
-   const markAsReadCount=document.getElementById('markAsReadCount');
-   // the main container
-   const rightSideBar=document.getElementById('rightSideBar');
-   const markAsReadArray=[];
-   
-   
 }
 
 loadPostData();
+
+//  user active status update
+const userActiveStatusUpdate=()=>{
+
+const postContents=document.querySelectorAll('.post-container');
+console.log(postContents.length);
+postContents.forEach((element)=>{
+  
+  const isActive=element.getAttribute('isactive')==='true';
+
+     if(isActive===false){
+      const indicators=element.querySelectorAll('.indicator-item');
+      indicators.forEach((indicator)=>{ 
+        indicator.classList.add('status-error');
+        indicator.classList.remove('status-success');
+
+      });
+      }
+});
+
+}
+
+// mark as read function 
+// collect the array of clicked mark as read button
+const markeAsReadPostsArrayPostId = [];
+const sideBarPostHandler=(postId)=>{
+   
+// console.log('user clicked',postId);
+
+// get the post that clicked mar as read
+const postMarkAsRead=document.getElementById(postId);
+console.log('user clicked ' ,postMarkAsRead);
+
+// when user click the envelope icon 
+const markAsReadCount=document.getElementById('markAsReadCount');
+
+// the main container
+const rightSideBar=document.getElementById('rightSideBar');
+
+if(!markeAsReadPostsArrayPostId.includes(postId)){
+
+ markeAsReadPostsArrayPostId.push(postId);
+//  update the mark as read counter
+ markAsReadCount.innerText=markeAsReadPostsArrayPostId.length;
+
+// clear the side bar
+const emptyMarkAsReadMessage=document.getElementById('emptyMarkAsReadMessage');
+emptyMarkAsReadMessage.classList.add('hidden');
+
+//  create the post that marked as readed
+ const markAsReadedPost=document.createElement('div');
+//  appended
+ rightSideBar.appendChild(markAsReadedPost);
+markAsReadedPost.setAttribute('class','animate-fade-in-up bg-white text-left rounded-xl flex justify-between items-center px-4 py-3 shadow flex-nowrap ');
+
+// getting the data of post that marked;
+const postTitle=postMarkAsRead.querySelector(`#post-title-${postId}`);
+const postTitleText=postTitle.innerText;
+
+const postViews=postMarkAsRead.querySelector(`#post-views-count-id-${postId}`);
+const postViewsCount=postViews.innerText;
+
+ markAsReadedPost.innerHTML=`
+ <h3 class="text-xl basis-[70%]">${postTitleText}</h3> 
+        <div class="flex justify-end items-center gap-1 flex-nowrap"><svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M11.6667 14C11.6667 14.6188 11.9125 15.2123 12.3501 15.6499C12.7877 16.0875 13.3812 16.3333 14 16.3333C14.6188 16.3333 15.2123 16.0875 15.6499 15.6499C16.0875 15.2123 16.3333 14.6188 16.3333 14C16.3333 13.3812 16.0875 12.7877 15.6499 12.3501C15.2123 11.9125 14.6188 11.6667 14 11.6667C13.3812 11.6667 12.7877 11.9125 12.3501 12.3501C11.9125 12.7877 11.6667 13.3812 11.6667 14Z" stroke="#12132D" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M24.5 14C21.7 18.6667 18.2 21 14 21C9.8 21 6.3 18.6667 3.5 14C6.3 9.33333 9.8 7 14 7C18.2 7 21.7 9.33333 24.5 14Z" stroke="#12132D" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+        <p class="text-lg text-gray-600 basis-[30%]">${postViewsCount}</p>
+        </div>
+ `;
+}
+else{
+  console.log(postMarkAsRead,'already ,marked as readed');
+} 
+ 
+}
+
 
 
